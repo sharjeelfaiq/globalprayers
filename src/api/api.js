911963609492e7e97fa2e2541901851e67865ed3
Sharjeel Faiq <mrmalik610@gmail.com>
2024-10-06 18:44:37 +0500

    Refactor API module to use axios for HTTP requests

--- src/api/api.js
+++ src/api/api.js
@@ -1,37 +1,42 @@
+import axios from "axios";
+
+// Define URLs as constants (could also use environment variables)
 const PRAYER_TIMES_URL = "https://api.aladhan.com/v1/calendarByCity";
 const ASMA_UL_HUSNA_URL = "https://api.aladhan.com/v1/asmaAlHusna/:number";
+
+const constructPrayerTimesUrl = ({
+  city,
+  country,
+  method,
+  school,
+  latitudeAdjustment,
+  midnightCalculation,
+}) => {
+  const date = new Date();
+  return `${PRAYER_TIMES_URL}/${date.getFullYear()}/${
+    date.getMonth() + 1
+  }?city=${city}&country=${country}&method=${method}&school=${school}&latitudeAdjustment=${latitudeAdjustment}&midnightCalculation=${midnightCalculation}`;
+};
 
 export const getData = {
   asmaUlHusma: async () => {
     const randomNumber = Math.floor(Math.random() * 99) + 1;
     const url = ASMA_UL_HUSNA_URL.replace(":number", randomNumber);
-    return await fetchData(url);
+    return fetchData(url);
   },
-  prayerTimes: async ({
-    city,
-    country,
-    method,
-    school,
-    latitudeAdjustment,
-    midnightCalculation,
-  }) => {
-    const date = new Date();
-    const url = `${PRAYER_TIMES_URL}/${date.getFullYear()}/${
-      date.getMonth() + 1
-    }?city=${city}&country=${country}&method=${method}&school=${school}&latitudeAdjustment=${latitudeAdjustment}&midnightCalculation=${midnightCalculation}`;
 
-    return await fetchData(url);
+  prayerTimes: async (params) => {
+    const url = constructPrayerTimesUrl(params);
+    return fetchData(url);
   },
 };
 
-async function fetchData(url) {
+const fetchData = async (url) => {
   try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-    const json = await response.json();
-    return json.data;
+    const { data } = await axios.get(url); // Destructure directly
+    return data.data; // Assuming the response structure remains the same
   } catch (error) {
     console.error("Failed to fetch data:", error);
     return null;
   }
-}
+};
